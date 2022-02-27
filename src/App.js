f38ff3e7db4f1020e0691c02029a514af161e785
OviiiOne <oviedo.97@hotmail.com>
2022-02-27 19:57:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import {
     BrowserRouter as Router,
     Routes,
     Route,
+    Navigate,
 } from "react-router-dom";
 import Box from "@material-ui/core/Box";
 import Home from "./Components/Home";
 import Callback from "./Components/Callback";
 import Form from "./Components/Form";
 import {Navigate} from "react-router-dom";
-import {Redirect} from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
 import Success from "./Components/Success";
 import Error from "./Components/Error";
