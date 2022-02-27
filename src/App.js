214b1bf2ccc2682e82c6bc19f6e0ff3774316fec
OviiiOne <oviedo.97@hotmail.com>
2022-02-27 19:59:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import Box from "@material-ui/core/Box";
 import Home from "./Components/Home";
 import Callback from "./Components/Callback";
 import Form from "./Components/Form";
-import {Navigate} from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
 import Success from "./Components/Success";
 import Error from "./Components/Error";
