78083cf3c5bcc898306e7fc1a0c2ba6f3d8d1893
OviiiOne <oviedo.97@hotmail.com>
2022-02-27 21:08:47 +0100

    intento de solucionar lo de netlify 2

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 module.exports = function override(config, env) {
     config.resolve.fallback = {
         url: require.resolve('url'),
@@ -18,4 +19,3 @@ module.exports = function override(config, env) {
     );
 
     return config;
-}
