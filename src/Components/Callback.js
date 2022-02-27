c2b765f7572af70889607387e3daed49592f115d
OviiiOne <oviedo.97@hotmail.com>
2022-02-27 19:36:47 +0100

    Update Callback.js

--- src/Components/Callback.js
+++ src/Components/Callback.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Redirect} from "react-router-dom";
+import {Navigate} from "react-router-dom";
 import {oauth} from"../App"
 class Callback extends Component {
 
